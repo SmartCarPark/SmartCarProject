abcb8b4661f0ee6af4661d33bbc57b786e022194
Ozan Yılmaz <ozanyilmaz2000@outlook.com>
2022-01-08 18:43:38 +0300

    update

--- DataAccess/Abstract/ISpotDal.cs
+++ DataAccess/Abstract/ISpotDal.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IReservatiınDal : IEntityRepository<Reservation>
+    public interface ISpotDal : IEntityRepository<Spot>
     {
     }
 }
--- DataAccess/Abstract/IUserDal.cs
+++ DataAccess/Abstract/IUserDal.cs
@@ -1,12 +1,12 @@
 ﻿using Core.DataAccess;
-using Entities.Concrete;
+using Core.Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IReservatiınDal : IEntityRepository<Reservation>
+    internal interface IUserDal : IEntityRepository<User>
     {
     }
 }
