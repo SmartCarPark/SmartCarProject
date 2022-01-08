b3e9a37335f5411e8fb826d512fc3e087c76e9ae
barış arslan <barisfiratarslan@hotmail.com>
2022-01-08 19:32:58 +0300

    u

---
+++ Business/Concrete/ParkManager.cs
@@ -0,0 +1,44 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ParkManager : IParkService
+    {
+        IParkDal _parkDal;
+
+        public ParkManager(IParkDal parkDal)
+        {
+            _parkDal = parkDal;
+        }
+
+        public Park GetByID(int id)
+        {
+            return _parkDal.Get(x => x.ParkId == id);
+        }
+
+        public List<Park> GetList()
+        {
+            return _parkDal.GetAll();
+        }
+
+        public void TAdd(Park t)
+        {
+            _parkDal.Add(t);
+        }
+
+        public void TDelete(Park t)
+        {
+            _parkDal.Delete(t);
+        }
+
+        public void TUpdate(Park t)
+        {
+            _parkDal.Update(t);
+        }
+    }
+}
