ca089b3c437f5c43546335ebd6c605e9ab630a5a
Ozan Yılmaz <ozanyilmaz2000@outlook.com>
2022-01-08 19:27:03 +0300

    update

---
+++ Business/Abstract/IReservationService.cs
@@ -0,0 +1,12 @@
+﻿using BusinessLayer.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IReservationService :  IGenericService<Reservation>
+    {
+    }
+}
---
+++ Business/Abstract/IUserservice.cs
@@ -0,0 +1,12 @@
+﻿using BusinessLayer.Abstract;
+using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IUserservice : IGenericService<User>
+    {
+    }
+}
