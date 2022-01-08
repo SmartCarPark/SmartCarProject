0b32d2025711f2b000347af149f3b65ac9a1877e
barış arslan <barisfiratarslan@hotmail.com>
2022-01-08 18:14:03 +0300

    Updated

---
+++ DataAccess/Concrete/Context.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-N7NUFR1;Database=SmartCarPark;Trusted_Connection=true");
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
