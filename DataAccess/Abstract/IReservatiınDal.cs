b493a73308c21914bcea8ba5e94a8e2a9bf6d916
Ozan Yılmaz <ozanyilmaz2000@outlook.com>
2022-01-08 18:40:02 +0300

    Updated

---
+++ DataAccess/Abstract/IReservatiınDal.cs
@@ -0,0 +1,11 @@
+﻿using Core.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    internal interface IReservatiınDal : IEntityRepository<Reservation>
+    {
+    }
+}
